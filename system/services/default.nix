5da982837b7041af19349f4b763eb0dfafecea53
h0useofdupree <joel.riekemann@gmail.com>
2025-01-16 14:39:05 +0100

    fix before-sleep by moving to system/services

--- system/services/default.nix
+++ system/services/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   services = {
     dbus.implementation = "broker";
 
@@ -7,6 +7,27 @@
       enable = true;
       # NOTE: Change this to lower values, if feasable, to reduce risk of data loss when system crashes
       resyncTimer = "10m";
+    };
+  };
+
+  # Use in place of hypridle's before_sleep_cmd, since systemd does not wait for
+  # it to complete
+  systemd.services.before-sleep = let
+    suspendScript = pkgs.writeShellScript "suspend-script" ''
+      # Lock all sessions
+      loginctl lock-sessions
+
+      # Wait for lockscreen
+      sleep 3
+    '';
+  in {
+    requiredBy = ["sleep.target"];
+    partOf = ["sleep.target"];
+    description = "Commands run before sleep";
+
+    serviceConfig = {
+      ExecStart = suspendScript.outPath;
+      Type = "oneshot";
     };
   };
 }
