85a8553fae16bdc4757180a70c8cc6f27683403c
h0useofdupree <joel.riekemann@gmail.com>
2025-01-16 14:41:24 +0100

    switch to powerManagement

--- system/services/default.nix
+++ system/services/default.nix
@@ -12,22 +12,14 @@
 
   # Use in place of hypridle's before_sleep_cmd, since systemd does not wait for
   # it to complete
-  systemd.services.before-sleep = let
-    suspendScript = pkgs.writeShellScript "suspend-script" ''
+  powerManagement = {
+    enable = true;
+    powerDownCommands = ''
       # Lock all sessions
       loginctl lock-sessions
 
       # Wait for lockscreen
-      sleep 3
+      sleep 1
     '';
-  in {
-    requiredBy = ["sleep.target"];
-    partOf = ["sleep.target"];
-    description = "Commands run before sleep";
-
-    serviceConfig = {
-      ExecStart = suspendScript.outPath;
-      Type = "oneshot";
-    };
   };
 }
