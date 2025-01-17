51982deafbbdc407148dc5c86da8d9caa2bda4f4
h0useofdupree <joel.riekemann@gmail.com>
2025-01-17 19:19:53 +0100

    sys: add udev rule for suspend wakeup problem

--- system/services/power.nix
+++ system/services/power.nix
@@ -5,5 +5,9 @@
     power-profiles-daemon.enable = true;
 
     upower.enable = true;
+
+    udev.extraRules = ''
+      ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1483" ATTR{power/wakeup}="disabled"
+    '';
   };
 }
