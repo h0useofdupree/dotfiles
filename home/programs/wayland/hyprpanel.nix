2c366ec79db00596c1491447eb0583b4505ae8c3
h0useofdupree <joel.riekemann@gmail.com>
2025-01-21 14:12:07 +0100

    hypr: enable hyprpanel

--- home/programs/wayland/hyprpanel.nix
+++ home/programs/wayland/hyprpanel.nix
@@ -1,8 +1,16 @@
-{...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   programs.hyprpanel = {
     enable = true;
     systemd.enable = true;
     hyprland.enable = true;
     overwrite.enable = true;
+    overlay.enable = true;
   };
+  pkgs.overlays = [
+    inputs.hyprpanel.overlay
+  ];
 }
