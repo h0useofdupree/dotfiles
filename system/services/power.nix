7f7b8456435edc2ee673fb8112b439a1ddb51dd0
h0useofdupree <joel.riekemann@gmail.com>
2025-01-21 13:32:01 +0100

    sys: add udev rules to desktop (correct) configuration

--- system/services/power.nix
+++ system/services/power.nix
@@ -1,3 +1,4 @@
+# Laptop only
 {
   services = {
     logind.powerKey = "suspend";
@@ -5,9 +6,5 @@
     power-profiles-daemon.enable = true;
 
     upower.enable = true;
-
-    udev.extraRules = ''
-      ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1483" ATTR{power/wakeup}="disabled"
-    '';
   };
 }
