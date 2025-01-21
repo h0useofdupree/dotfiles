f853dbee041207a688c9c4058b65961feb771506
h0useofdupree <joel.riekemann@gmail.com>
2025-01-21 12:54:30 +0100

    sys: add udev suspend rule

--- system/services/power.nix
+++ system/services/power.nix
@@ -6,8 +6,8 @@
 
     upower.enable = true;
 
-    #udev.extraRules = ''
-    #  ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1483" ATTR{power/wakeup}="disabled"
-    #'';
+    udev.extraRules = ''
+      ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1483" ATTR{power/wakeup}="disabled"
+    '';
   };
 }
