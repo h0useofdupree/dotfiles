acad54b62ccd9f80b3e75c89bd50cba3ee8d98c0
h0useofdupree <joel.riekemann@gmail.com>
2025-01-22 16:15:12 +0100

    test nix syntax

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -1,7 +1,11 @@
-{...}: {
+_: {
   # Graphic drivers / HW accel
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+
+    # extraPackages = with pkgs; [
+    #   amdvlk
+    # ];
   };
 }
