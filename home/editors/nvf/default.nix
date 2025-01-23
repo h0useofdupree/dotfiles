c220e5be92fe69aacbe26836d64e62b14abe17f3
h0useofdupree <joel.riekemann@gmail.com>
2025-01-23 22:07:16 +0100

    nvf: default (maximal) config

--- home/editors/nvf/default.nix
+++ home/editors/nvf/default.nix
@@ -1,5 +1,208 @@
 {
   programs.nvf = {
     enable = true;
+    config.vim = {
+      viAlias = true;
+      vimAlias = true;
+      debugMode = {
+        enable = false;
+        level = 16;
+        logFile = "/tmp/nvim.log";
+      };
+
+      spellcheck = {
+        enable = true;
+      };
+
+      lsp = {
+        formatOnSave = true;
+        lspkind.enable = false;
+        lightbulb.enable = true;
+        lspsaga.enable = false;
+        trouble.enable = true;
+        lspSignature.enable = true;
+        otter-nvim.enable = true;
+        lsplines.enable = true;
+        nvim-docs-view.enable = true;
+      };
+
+      debugger = {
+        nvim-dap = {
+          enable = true;
+          ui.enable = true;
+        };
+      };
+
+      languages = {
+        enableLSP = true;
+        enableFormat = true;
+        enableTreesitter = true;
+        enableExtraDiagnostics = true;
+
+        nix.enable = true;
+        markdown.enable = true;
+        bash.enable = true;
+        clang.enable = true;
+        css.enable = true;
+        html.enable = true;
+        sql.enable = true;
+        java.enable = true;
+        kotlin.enable = true;
+        ts.enable = true;
+        go.enable = true;
+        lua.enable = true;
+        zig.enable = true;
+        python.enable = true;
+        typst.enable = true;
+        rust = {
+          enable = true;
+          crates.enable = true;
+        };
+
+        assembly.enable = false;
+        astro.enable = false;
+        nu.enable = false;
+        csharp.enable = false;
+        julia.enable = false;
+        vala.enable = false;
+        scala.enable = false;
+        r.enable = false;
+        gleam.enable = false;
+        dart.enable = false;
+        ocaml.enable = false;
+        elixir.enable = false;
+        haskell.enable = false;
+        ruby.enable = false;
+
+        tailwind.enable = false;
+        svelte.enable = false;
+        nim.enable = false;
+      };
+
+      visuals = {
+        nvim-scrollbar.enable = true;
+        nvim-web-devicons.enable = true;
+        nvim-cursorline.enable = true;
+        cinnamon-nvim.enable = true;
+        fidget-nvim.enable = true;
+        highlight-undo.enable = true;
+        indent-blankline.enable = true;
+      };
+
+      statusline = {
+        lualine = {
+          enable = true;
+          theme = "catppuccin";
+        };
+      };
+
+      theme = {
+        enable = true;
+        name = "catppuccin";
+        style = "mocha";
+        transparent = false;
+      };
+
+      autopairs.nvim-autopairs.enable = true;
+
+      autocomplete.nvim-cmp.enable = true;
+      snippets.luasnip.enable = true;
+
+      filetree = {
+        neo-tree = {
+          enable = true;
+        };
+      };
+
+      tabline = {
+        nvimBufferline.enable = true;
+      };
+
+      treesitter.context.enable = true;
+
+      binds = {
+        whichKey.enable = true;
+        cheatsheet.enable = true;
+      };
+
+      telescope.enable = true;
+
+      git = {
+        enable = true;
+        gitsigns.enable = true;
+        gitsigns.codeActions.enable = false;
+      };
+
+      minimap = {
+        codewindow.enable = true;
+      };
+
+      dashboard = {
+        alpha.enable = true;
+      };
+
+      notify = {
+        nvim-notify.enable = true;
+      };
+
+      projects = {
+        project-nvim.enable = true;
+      };
+
+      utility = {
+        icon-picker.enable = true;
+        surround.enable = true;
+        diffview-nvim.enable = true;
+        motion = {
+          hop.enable = true;
+          leap.enable = true;
+          precognition.enable = true;
+        };
+      };
+
+      notes = {
+        mind-nvim.enable = true;
+        todo-comments.enable = true;
+      };
+
+      terminal = {
+        toggleterm = {
+          enable = true;
+          lazygit.enable = true;
+        };
+      };
+
+      ui = {
+        borders.enable = true;
+        noice.enable = true;
+        colorizer.enable = true;
+        illuminate.enable = true;
+        breadcrumbs = {
+          enable = true;
+          navbuddy.enable = true;
+        };
+        smartcolumn = {
+          enable = true;
+          setupOpts.custom_colorcolumn = {
+            nix = "110";
+            ruby = "120";
+            java = "130";
+            go = ["90" "130"];
+          };
+        };
+        fastaction.enable = true;
+      };
+
+      assistant = {
+        copilot = {
+          enable = false;
+          cmp.enable = true;
+        };
+      };
+
+      comments = {
+        comment-nvim.enable = true;
+      };
+    };
   };
 }
