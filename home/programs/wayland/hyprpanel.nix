c9c3f8ee4a47349cbf9b2b7b64ce95e156975b93
h0useofdupree <joel.riekemann@gmail.com>
2025-01-25 23:18:23 +0100

    home/hyprpanel: add more configuration

--- home/programs/wayland/hyprpanel.nix
+++ home/programs/wayland/hyprpanel.nix
@@ -5,5 +5,27 @@
     hyprland.enable = true;
     overwrite.enable = true;
     overlay.enable = true;
+
+    theme = "rose_pine";
+
+    layout = {
+      "bar.layouts" = {
+        "0" = {
+          left = ["dashboard" "workspaces" "media"];
+          middle = ["windowtitle"];
+          right = ["volume" "bluetooth" "network" "power" "systray" "clock" "notifications"];
+        };
+      };
+    };
+
+    settings = {
+      menus = {
+        clock.weather = {
+          enabled = true;
+          location = "Mettmann";
+          unit = "metric";
+        };
+      };
+    };
   };
 }
