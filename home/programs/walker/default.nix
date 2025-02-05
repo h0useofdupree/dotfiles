4fc46fa928015f9bc7f83541e6ef0571505c5e10
h0useofdupree <joel.riekemann@gmail.com>
2025-02-05 19:08:16 +0100

    h/walker: basic setup

--- home/programs/walker/default.nix
+++ home/programs/walker/default.nix
@@ -6,7 +6,22 @@
     runAsService = true;
 
     config = {
+      app_launch_prefix = "uwsm app --";
+      close_when_open = false;
+      timeout = 5;
+
+      activation_mode.disabled = true;
+
+      ui.fullscreen = true;
+
       search.placeholder = "make a wish...";
+
+      list = {
+        height = 200;
+      };
+
+      websearch.prefix = "?";
+      switcher.prefix = "/";
     };
   };
 }
