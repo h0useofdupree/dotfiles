52deca132eeaebfc1f5d819bad84ae28f05129aa
h0useofdupree <joel.riekemann@gmail.com>
2025-06-25 10:34:40 +0200

    sys/hyprland: enable withUWSM

--- system/programs/hyprland.nix
+++ system/programs/hyprland.nix
@@ -5,6 +5,7 @@
 }: {
   programs.hyprland = {
     enable = true;
+    withUWSM = true;
 
     package = inputs.hyprland.packages.${pkgs.system}.default;
     portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
