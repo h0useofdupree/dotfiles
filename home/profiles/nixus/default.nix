43bb82dd9e5c3002ad9c2a75c0c735987793f9e0
h0useofdupree <joel.riekemann@gmail.com>
2025-06-25 11:11:28 +0200

    h/profiles: fix nixus hyprland display

--- home/profiles/nixus/default.nix
+++ home/profiles/nixus/default.nix
@@ -33,7 +33,7 @@
 
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "DP-1, 3440x1440@160, auto, 1"
+      "DP-1, 3440x1440@144, auto, 1"
       ", preferred, auto, 1"
     ];
   };
