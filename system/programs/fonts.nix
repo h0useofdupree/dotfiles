649047362b2b5240881eccaf2d654e3277074590
h0useofdupree <joel.riekemann@gmail.com>
2025-06-25 10:10:37 +0200

    sys/fonts: improve emojis

--- system/programs/fonts.nix
+++ system/programs/fonts.nix
@@ -17,9 +17,17 @@
 
       # Nerd-Fonts
       nerd-fonts.caskaydia-cove
+      nerd-fonts.jetbrains-mono
       nerd-fonts.symbols-only
     ];
 
     enableDefaultPackages = false;
+
+    fontconfig.defaultFonts = {
+      serif = ["Libertinus Serif"];
+      sansSerif = ["Inter"];
+      monospace = ["JetBrains Mono Nerd Font"];
+      emoji = ["Noto Color Emoji"];
+    };
   };
 }
