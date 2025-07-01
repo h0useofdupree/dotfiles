7e887a6b15a079d9e953ae538f7ed7c4eb94ee4b
h0useofdupree <joel.riekemann@gmail.com>
2025-07-01 13:01:30 +0200

    pkgs/speakerctl: disable checks and set format correctly

--- pkgs/speakerctl/default.nix
+++ pkgs/speakerctl/default.nix
@@ -3,6 +3,8 @@ python3Packages.buildPythonApplication {
   pname = "speakerctl";
   version = "1.0.0";
 
+  format = "other";
+
   src = ./.;
 
   propagatedBuildInputs = [python3Packages.tinytuya];
@@ -11,4 +13,6 @@ python3Packages.buildPythonApplication {
     mkdir -p $out/bin
     install -m755 main.py $out/bin/speakerctl
   '';
+
+  doCheck = false;
 }
