36b8720b22cf06185182aa114d0563a59fced34b
h0useofdupree <joel.riekemann@gmail.com>
2025-07-02 13:08:24 +0200

    modules/speakerctl: remove devices options

--- modules/speakerctl/default.nix
+++ modules/speakerctl/default.nix
@@ -14,36 +14,11 @@ in {
       default = false;
       description = "Enable smart speaker control.";
     };
-
-    devices = mkOption {
-      type = types.listOf types.attrs;
-      default = [];
-      description = "Device definitions written to devices.json.";
-      example = [
-        {
-          id = "device-id";
-          ip = "192.168.1.10";
-          key = "abcd";
-          version = 3.3;
-        }
-      ];
-    };
-
-    devicesFile = mkOption {
-      type = with types; nullOr path;
-      default = null;
-      description = "Path to an existing devices.json file.";
-    };
   };
 
   config = mkIf cfg.enable {
     home.packages = [
       inputs.self.packages.${pkgs.system}.speakerctl
     ];
-
-    home.file.".config/speakerctl/devices.json" =
-      if cfg.devicesFile != null
-      then {source = cfg.devicesFile;}
-      else {text = builtins.toJSON cfg.devices;};
   };
 }
