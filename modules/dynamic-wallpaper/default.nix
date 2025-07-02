e2e34a05fa2acc9adf6de62618860272b934a317
h0useofdupree <joel.riekemann@gmail.com>
2025-07-02 23:04:29 +0200

    modules: add dynamic-wallpaper module with service/timer

---
+++ modules/dynamic-wallpaper/default.nix
@@ -0,0 +1,67 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+with lib; let
+  cfg = config.dynamicWallpaper;
+  defaultGroup = config.home.homeDirectory + "/.local/share/dynamic-wallpaper/default";
+in {
+  options.dynamicWallpaper = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable dynamic wallpaper service";
+    };
+    group = mkOption {
+      type = types.str;
+      default = defaultGroup;
+      description = "Directory containing wallpaper images.";
+    };
+    autoLight = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Use the lightest wallpaper when GNOME is set to light mode.";
+    };
+
+    refreshInterval = mkOption {
+      type = types.str;
+      default = "30m";
+      description = "How often to refresh the wallpaper.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [
+      inputs.self.packages.${pkgs.system}.dynamic-wallpaper
+    ];
+
+    home.sessionVariables = {
+      DYNAMIC_WALLPAPER_DIR = cfg.group;
+      DYNAMIC_WALLPAPER_AUTO_LIGHT =
+        if cfg.autoLight
+        then "1"
+        else "0";
+    };
+
+    systemd.user.services.dynamic-wallpaper = {
+      Unit.Description = "Update dynamic wallpaper";
+      Service = {
+        Type = "oneshot";
+        ExecStart = lib.getExe inputs.self.packages.${pkgs.system}.dynamic-wallpaper;
+      };
+    };
+
+    systemd.user.timers.dynamic-wallpaper = {
+      Unit.Description = "Schedule dynamic wallpaper refresh";
+      Timer = {
+        OnBootSec = "1m";
+        OnUnitActiveSec = cfg.refreshInterval;
+      };
+      Timer.Unit = "dynamic-wallpaper.service";
+      Install.WantedBy = ["graphical-session.target"];
+    };
+  };
+}
