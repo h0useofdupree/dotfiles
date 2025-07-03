e9fc10271b81e1a9465d7501c5a5ae80152e3319
h0useofdupree <joel.riekemann@gmail.com>
2025-07-03 13:01:56 +0200

    h/s/w/dynamic-wallpaper: add favs and adjust interval

--- home/services/wayland/dynamic-wallpaper.nix
+++ home/services/wayland/dynamic-wallpaper.nix
@@ -3,7 +3,12 @@
   dynamicWallpaper = {
     enable = true;
     autoLight = true;
-    refreshInterval = "30m";
-    group = config.home.homeDirectory + "/.local/share/dynamic-wallpapers/WaterHill";
+    refreshInterval = "5m";
+    group = config.home.homeDirectory + "/.local/share/dynamic-wallpapers/Mojave";
+    # Favorites
+    # - DesertSands - 5 images - Ultrawide
+    # - Mojave - 16 images - 4K
+    # - WaterHill - 2 images - 4K
+    # - Ocean - 2 images - 4K
   };
 }
