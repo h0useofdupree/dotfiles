74cc075af183236951d8f9ba2f2352afdb99f497
h0useofdupree <joel.riekemann@gmail.com>
2025-07-09 19:35:41 +0200

    hosts/linx/hyprland: add MacOS style accelpoints

--- hosts/linx/hyprland.nix
+++ hosts/linx/hyprland.nix
@@ -4,6 +4,7 @@ let
     kb_variant = "altgr-intl";
     kb_options = "compose:caps, level3:ralt_switch";
   };
+  accelpoints = "0.5 0.000 0.053 0.115 0.189 0.280 0.391 0.525 0.687 0.880 1.108 1.375 1.684 2.040 2.446 2.905 3.422 4.000 4.643 5.355 6.139";
 in {
   programs.hyprland.settings = {
     monitor = [
@@ -13,5 +14,10 @@ in {
     ];
     "device[keychron-keychron-q1]" = mkKeyboard;
     "device[keychron-keychron-q1-keyboard]" = mkKeyboard;
+    "device[dll0945:00-04f3:311c-touchpad]" = {
+      accel_profile = "custom ${accelpoints}";
+      scroll_points = accelpoints;
+      natural_scroll = true;
+    };
   };
 }
