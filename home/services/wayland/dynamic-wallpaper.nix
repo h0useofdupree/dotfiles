e592263f38a1828f97b5891dc26ab06504155642
h0useofdupree <joel.riekemann@gmail.com>
2025-07-09 19:35:15 +0200

    h/serv/w/dynamic-wallpaper: disable autoLight

--- home/services/wayland/dynamic-wallpaper.nix
+++ home/services/wayland/dynamic-wallpaper.nix
@@ -2,7 +2,7 @@
   services.swww.enable = true;
   dynamicWallpaper = {
     enable = true;
-    autoLight = true;
+    autoLight = false;
     refreshInterval = "5m";
     group = config.home.homeDirectory + "/.local/share/dynamic-wallpapers/Mojave";
     # Favorites
