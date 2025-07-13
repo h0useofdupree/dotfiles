1e8ccf7ead2af397da621e650832ffaa568c8ecf
h0useofdupree <joel.riekemann@gmail.com>
2025-07-13 22:11:04 +0200

    h/services/gammastep: add actual coordinates (bruh)

--- home/services/wayland/gammastep.nix
+++ home/services/wayland/gammastep.nix
@@ -3,10 +3,9 @@
     enable = true;
     tray = true;
 
-    # stopgap until geoclue's wifi location is fixed
     provider = "manual";
-    latitude = 45.0;
-    longitude = 25.0;
+    latitude = 51.1;
+    longitude = 7.3;
 
     enableVerboseLogging = true;
 
