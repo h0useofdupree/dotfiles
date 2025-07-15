e4594092469db38fe31c8273be72dc7ff3cb1ca2
h0useofdupree <joel.riekemann@gmail.com>
2025-07-15 21:25:45 +0200

    h/p/w/hyprpanel: complete redesign and config structure

--- home/programs/wayland/hyprpanel.nix
+++ home/programs/wayland/hyprpanel.nix
@@ -4,6 +4,7 @@
     systemd.enable = true;
 
     settings = {
+      # Bar: Layouts
       "bar.layouts" = {
         "*" = {
           left = ["dashboard" "workspaces" "media"];
@@ -11,38 +12,84 @@
           right = ["volume" "bluetooth" "network" "battery" "systray" "notifications" "clock"];
         };
       };
-      "tear" = true;
-      "bar.customModules.cava.showIcon" = false;
-      "bar.customModules.cava.showActiveOnly" = true;
-      "bar.clock.format" = "%a %b %d %H:%M";
-      "menus.clock.time.hideSeconds" = false;
-      "menus.clock.weather" = {
-        enabled = true;
-        location = "Mettmann";
-        unit = "metric";
-        key = "55b82b6781af4a4c9b2165259221802";
+      bar = {
+        clock.format = "%a %b %d %H:%M";
+        customModules = {
+          cava = {
+            showIcon = false;
+            showActiveOnly = true;
+          };
+        };
+        launcher.icon = "";
+        # TODO: Find out where to put these
+        shadow = "0px 0px 0px 0px #16161e";
+        shadowMargins = "10px 10px";
       };
-      "menus.dashboard.shortcuts.enabled" = true;
-      "menus.dashboard.stats.enabled" = true;
-      "menus.dashboard.stats.enable_gpu" = false;
-      "menus.dashboard.stats.interval" = 2000;
+      tear = true;
 
-      "theme.bar.location" = "top";
-      "theme.bar.floating" = false;
-      "theme.bar.margin_sides" = "1.5em";
-      "theme.bar.transparent" = true;
-      "theme.bar.outer_spacing" = "0.3em";
-      "theme.bar.border_radius" = "1.0em";
-      "theme.bar.buttons.enableBorders" = true;
-      "theme.bar.buttons.radius" = "0.85em";
-      "theme.bar.buttons.spacing" = "1.0em";
-      "theme.bar.buttons.padding_x" = "1.0em";
-      "theme.bar.workspaces.showApplicationIcons" = true;
+      menus = {
+        clock = {
+          time.hideSeconds = false;
+          weather = {
+            enabled = true;
+            location = "Mettmann";
+            unit = "metric";
+            key = "55b82b6781af4a4c9b2165259221802";
+          };
+        };
 
-      "bar.launcher.icon" = "";
-      "wallpaper.enable" = false;
-      "wallpaper.image" = "/home/h0useofdupree/.local/share/dynamic-wallpapers/Mojave/mojave_dynamic_1.jpeg";
-      "theme.matugen" = true;
+        dashboard = {
+          # "menus.dashboard.powermenu.avatar.image" = "/home/h0useofdupree/.cache/dynamic-wallpaper/current"; # Use default: ~/.face.icon
+          # TODO: Encrypt ~/.face.icon with age(nix) and add to repo
+          shortcuts.enabled = true;
+          stats = {
+            enabled = true;
+            enable_gpu = false;
+            interval = 2000;
+          };
+        };
+      };
+
+      wallpaper = {
+        enable = false;
+        # Choose image for matugen
+        image = "/home/h0useofdupree/.cache/dynamic-wallpaper/current";
+      };
+      theme = {
+        matugen = true;
+        matugen_settings = {
+          mode = "dark";
+          scheme_type = "monochrome";
+          variation = "standard_1";
+        };
+        bar = {
+          border_radius = "1.0em";
+          buttons = {
+            enableBorders = false;
+            radius = "0.85em";
+            spacing = "0.3em";
+            padding_x = "1.0em";
+            background_opacity = "0";
+          };
+          floating = false;
+          location = "top";
+          margin_sides = "1.5em";
+          menus = {
+            border.radius = "1.5em";
+            card_radius = "1.5em";
+            popover.radius = "1.5em";
+            opacity = "60";
+          };
+          opacity = "30";
+          outer_spacing = "0.5em";
+          transparent = false;
+          workspaces.showApplicationIcons = true;
+        };
+        osd = {
+          location = "top";
+          orientation = "horizontal";
+        };
+      };
     };
   };
 }
