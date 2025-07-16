74c48609af8f0eb5713848c8c29addc3d43fef2d
h0useofdupree <joel.riekemann@gmail.com>
2025-07-16 13:45:50 +0200

    secrets: add linx pub key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   h0useofdupree = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC1JYHp/ZXHErtQVer2eE393NoJgOB6LvVJ+x/IxayS9 joel.riekemann@gmail.com";
   nixus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINFXgN3Ckv//Fk+YALU5f5/bkc5ThavWvLdEqjwxgFgS root@nixus";
-  # linx = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUt3ZeBEajsvYdWePId2leizebVy+awgNaZPcrIWngi root@nixos";
+  linx = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUt3ZeBEajsvYdWePId2leizebVy+awgNaZPcrIWngi root@nixos";
 in {
   "speakerctl-devices.age".publicKeys = [h0useofdupree nixus];
 }
