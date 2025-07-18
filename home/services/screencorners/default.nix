4a93411f535c33dccfed06058afe23081b525345
h0useofdupree <joel.riekemann@gmail.com>
2025-07-18 11:51:25 +0200

    fix(screencorners): create correct service

--- home/services/screencorners/default.nix
+++ home/services/screencorners/default.nix
@@ -15,43 +15,20 @@
     };
   };
 
-  #   systemd.user.services.screencorners = {
-  #     Unit = {
-  #       Description = "AGS Screencorners service";
-  #       After = ["hyprland-session.target"];
-  #       PartOf = ["graphical-session.target"];
-  #       ConditionEnvironment = "WAYLAND_DISPLAY";
-  #     };
-  #
-  #     Service = {
-  #       Type = "simple";
-  #
-  #       # Wait until hyprland reports monitors
-  #       ExecStartPre = pkgs.writeShellScript "wait-for-hyprland" ''
-  #         export PATH=${pkgs.hyprland}/bin:${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:${pkgs.findutils}/bin:$PATH
-  #
-  #         echo "Waiting for HYPRLAND_INSTANCE_SIGNATURE..."
-  #         while [ -z "$HYPRLAND_INSTANCE_SIGNATURE" ]; do
-  #           sleep 0.2
-  #         done
-  #
-  #         echo "Waiting for at least one monitor..."
-  #         while ! hyprctl monitors | grep -q "^Monitor .* (ID [0-9]\+):"; do
-  #           sleep 0.2
-  #         done
-  #
-  #         echo "Monitors detected, starting AGS..."
-  #       '';
-  #
-  #       ExecStart = "${pkgs.hyprpanel}/bin/ags";
-  #       ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
-  #       Restart = "always";
-  #       RestartSec = 2;
-  #       KillMode = "mixed";
-  #     };
-  #
-  #     Install = {
-  #       WantedBy = ["graphical-session.target"];
-  #     };
-  #   };
+  systemd.user.services.ags-corners = {
+    Unit = {
+      Description = "Aylur's GTK Shell";
+      PartOf = [
+        # "tray.target"
+        "graphical-session.target"
+      ];
+      After = "graphical-session.target";
+    };
+
+    Service = {
+      ExecStart = "${inputs.ags.packages.${pkgs.system}.ags}/bin/ags";
+      Restart = "on-failure";
+    };
+    Install.WantedBy = ["graphical-session.target"];
+  };
 }
