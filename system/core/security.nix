23a6e5c3ee787acd47dfc9f5f756252607a3075c
h0useofdupree <joel.riekemann@gmail.com>
2025-07-21 13:59:34 +0200

    feat(security): add hardened security settings

--- system/core/security.nix
+++ system/core/security.nix
@@ -1,11 +1,4 @@
-# Core settings to consider
-
-## General Security
-
-- hlissner's security tweaks (see fufexan sys/core/security.nix
-
-```nix
-# security tweaks borrowed from @hlissner
+# security tweaks borrowed from @fufexan from @hlissner
 {
   boot.kernel.sysctl = {
     # The Magic SysRq key is a key combo that allows users connected to the
@@ -16,16 +9,20 @@
     ## TCP hardening
     # Prevent bogus ICMP errors from filling up logs.
     "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
+
     # Reverse path filtering causes the kernel to do source validation of
     # packets received from all interfaces. This can mitigate IP spoofing.
     "net.ipv4.conf.default.rp_filter" = 1;
     "net.ipv4.conf.all.rp_filter" = 1;
+
     # Do not accept IP source route packets (we're not a router)
     "net.ipv4.conf.all.accept_source_route" = 0;
     "net.ipv6.conf.all.accept_source_route" = 0;
+
     # Don't send ICMP redirects (again, we're not a router)
     "net.ipv4.conf.all.send_redirects" = 0;
     "net.ipv4.conf.default.send_redirects" = 0;
+
     # Refuse ICMP redirects (MITM mitigations)
     "net.ipv4.conf.all.accept_redirects" = 0;
     "net.ipv4.conf.default.accept_redirects" = 0;
@@ -33,8 +30,10 @@
     "net.ipv4.conf.default.secure_redirects" = 0;
     "net.ipv6.conf.all.accept_redirects" = 0;
     "net.ipv6.conf.default.accept_redirects" = 0;
+
     # Protects against SYN flood attacks
     "net.ipv4.tcp_syncookies" = 1;
+
     # Incomplete protection again TIME-WAIT assassination
     "net.ipv4.tcp_rfc1337" = 1;
 
@@ -43,6 +42,7 @@
     # data in the sender’s initial TCP SYN. Setting 3 = enable TCP Fast Open for
     # both incoming and outgoing connections:
     "net.ipv4.tcp_fastopen" = 3;
+
     # Bufferbloat mitigations + slight improvement in throughput & latency
     "net.ipv4.tcp_congestion_control" = "bbr";
     "net.core.default_qdisc" = "cake";
@@ -57,48 +57,15 @@
     # userland niceness
     rtkit.enable = true;
 
-    # don't ask for password for wheel group
-    sudo.wheelNeedsPassword = false;
-  };
-}
-```
-
-## Secure Boot
-
-- lanzaboote (see fufexan sys/core/lanzaboote.nix
-- needs systemd-boot enabled (consider this!)
-- might be good for linx (laptop)
-
-```nix
-{
-  pkgs,
-  lib,
-  inputs,
-  ...
-}:
-# lanzaboote config
-{
-  imports = [
-    inputs.lanzaboote.nixosModules.lanzaboote
-  ];
-
-  boot = {
-    lanzaboote = {
+    sudo-rs = {
       enable = true;
-      pkiBundle = "/etc/secureboot";
+      wheelNeedsPassword = false;
+
+      extraConfig = ''
+        Defaults lecture = never
+        Defaults pwfeedback
+        Defaults timestamp_timeout = 300
+      '';
     };
-
-    # we let lanzaboote install systemd-boot
-    loader.systemd-boot.enable = lib.mkForce false;
   };
-
-  environment.systemPackages = [pkgs.sbctl];
 }
-```
-
-- bootspec (see fufexan /sys/core/boot.nix)
-- needs systemd-boot enabled (consider this!)
-
-```nix
-boot.bootspec.enable = true;
-```
