6f5970b99567b2f074660eac51140e84426241b4
h0useofdupree <joel.riekemann@gmail.com>
2025-07-27 15:38:12 +0200

    feat(zen): add tridactyl-native

--- home/programs/browsers/zen.nix
+++ home/programs/browsers/zen.nix
@@ -5,5 +5,8 @@
 }: {
   home.packages = [
     inputs.zen-browser.packages.${pkgs.system}.default
+    pkgs.tridactyl-native
   ];
+
+  home.file.".mozilla/native-messaging-hosts/tridactyl.json".source = "${pkgs.tridactyl-native}/lib/mozilla/native-messaging-hosts/tridactyl.json";
 }
