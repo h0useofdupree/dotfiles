bdb0dd428205449c345a0970e02a8795e58f2c01
h0useofdupree <joel.riekemann@gmail.com>
2025-08-01 23:43:53 +0200

    fix(graphics): use hyprland mesa pkg
    this fixes the latest-commit startup crash

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -1,11 +1,26 @@
-_: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  pkgs-unstable = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in {
   # Graphic drivers / HW accel
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
 
-    # extraPackages = with pkgs; [
-    #   amdvlk
-    # ];
+    package = pkgs-unstable.mesa;
+    package32 = pkgs-unstable.pkgsi686Linux.mesa;
+
+    extraPackages = with pkgs; [
+      libva # VA-API video accleration
+      vaapiVdpau # Legacy bridge
+      # amdvlk
+    ];
+
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      libva
+    ];
   };
 }
