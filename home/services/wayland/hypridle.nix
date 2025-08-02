e23db12cc4f50c1dccc33601da9607f2b246d117
h0useofdupree <joel.riekemann@gmail.com>
2025-08-02 16:48:58 +0200

    feat(hypridle): use screen dimming when on laptop

--- home/services/wayland/hypridle.nix
+++ home/services/wayland/hypridle.nix
@@ -3,11 +3,12 @@
   lib,
   config,
   inputs,
+  isLaptop ? false,
   ...
 }: let
   lock = "${pkgs.systemd}/bin/loginctl lock-session";
 
-  # brillo = lib.getExe pkgs.brillo;
+  brillo = lib.getExe pkgs.brillo;
 
   # 20min
   timeout = 1200;
@@ -24,25 +25,27 @@ in {
         lock_cmd = "pgrep hyprlock || ${lib.getExe config.programs.hyprlock.package}";
       };
 
-      listener = [
-        {
-          #TODO: Enable only for linx (laptop) and maybe for nixus (with ddc? meh)
-          # timeout = timeout - 10;
-          # # save the current brightness and dim the screen over a period of 500ms
-          # on-timeout = "${brillo} -O; ${brillo} -u 500000 -S 10";
-          # # brighten the screen over a period of 250ms to the saved value
-          # on-resume = "${brillo} -I -u 250000";
-        }
-        {
-          inherit timeout;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
-        {
-          timeout = timeout + 10;
-          on-timeout = lock;
-        }
-      ];
+      listener =
+        lib.optionals isLaptop [
+          {
+            timeout = timeout - 10;
+            # save the current brightness and dim the screen over a period of 500ms
+            on-timeout = "${brillo} -O; ${brillo} -u 500000 -S 10";
+            # brighten the screen over a period of 250ms to the saved value
+            on-resume = "${brillo} -I -u 250000";
+          }
+        ]
+        ++ [
+          {
+            inherit timeout;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = timeout + 10;
+            on-timeout = lock;
+          }
+        ];
     };
   };
   # systemd.user.services.hypridle.Unit.After = lib.mkForce "graphical-session.target";
