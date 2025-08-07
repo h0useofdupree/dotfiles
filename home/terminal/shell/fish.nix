db9560b3138cd5cc3a1a185057893ecb49eb8723
h0useofdupree <joel.riekemann@gmail.com>
2025-08-07 14:24:05 +0200

    refactor(fish): move nix completions to fish_completions/default

--- home/terminal/shell/fish.nix
+++ home/terminal/shell/fish.nix
@@ -7,7 +7,6 @@
   imports = [
     ./fish_completions
   ];
-  xdg.configFile."fish/completions/nix.fish".source = "${pkgs.nix}/share/fish/vendor_completions.d/nix.fish";
   programs.fish = {
     enable = true;
     generateCompletions = true;
--- home/terminal/shell/fish_completions/default.nix
+++ home/terminal/shell/fish_completions/default.nix
@@ -1,3 +1,4 @@
-{
+{pkgs, ...}: {
   xdg.configFile."fish/completions/bat.fish".source = ./bat.fish;
+  xdg.configFile."fish/completions/nix.fish".source = "${pkgs.nix}/share/fish/vendor_completions.d/nix.fish";
 }
