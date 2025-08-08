51c56d7c503c2f236dec29214aee4eadcaa8216d
h0useofdupree <joel.riekemann@gmail.com>
2025-08-08 18:51:56 +0200

    style(misc): clean up

--- home/services/wayland/dynamic-wallpaper.nix
+++ home/services/wayland/dynamic-wallpaper.nix
@@ -6,19 +6,15 @@
     startTime = "06:00";
     endTime = "23:00";
     refreshInterval = "5m";
-    group = "ZorinMountain";
+    group = "Fuji";
     currentLink = config.home.homeDirectory + "/.cache/dynamic-wallpaper/current";
 
-    # NOTE: Available sets:
-    # DesertSands
-    # -> 5 images - Ultrawide
-    # Mojave
-    # -> 16 images - 4K
-    # WaterHill
-    # -> 2 images - 4K
-    # Ocean
-    # -> 2 images - 4K
-    # ZorinMountain
-    # -> 5 images - 5k
+    # NOTE: Available groups:
+    # - 'DesertSands' - 5 images - 5160x2160
+    # - 'Fuji' - 7 images - 5719x3720
+    # - 'Mojave' - 16 images - 5120x2880
+    # - 'Ocean' - 2 images - 4096x2621
+    # - 'WaterHill' - 2 images - 4096x2621
+    # - 'ZorinMountain' - 5 images - 5760x3600
   };
 }
