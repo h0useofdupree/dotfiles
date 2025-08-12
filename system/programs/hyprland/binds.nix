fe0cc43375b411d5ee08271a864b1c829ae55803
h0useofdupree <joel.riekemann@gmail.com>
2025-08-12 20:36:34 +0200

    fix(hyprland): fix caelestia lock bind
    don't ever try to run a global command with uwsm... makes no sense

--- system/programs/hyprland/binds.nix
+++ system/programs/hyprland/binds.nix
@@ -92,7 +92,7 @@ in {
         # lock screen
         "$mod $mod2, L, exec, ${runOnce "hyprlock"}"
         # "$mod, L, global, systemctl --user start caelestia-shell.service"
-        "$mod2, L, global, ${runOnce "caelestia:lock"}"
+        "$mod2, L, global, caelestia:lock"
 
         # select area to perform OCR on
         "$mod, O, exec, ${runOnce "wl-ocr"}"
