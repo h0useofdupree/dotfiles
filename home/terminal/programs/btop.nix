ed703a391ada63ee3ab1c2e3d86af1dd4d018dc5
h0useofdupree <joel.riekemann@gmail.com>
2025-08-16 15:28:46 +0200

    feat(terminal/programs): use btop-{cuda,rocm} depending on host

--- home/terminal/programs/btop.nix
+++ home/terminal/programs/btop.nix
@@ -1,5 +1,18 @@
-_: {
-  programs.btop = {
-    enable = true;
+{
+  pkgs,
+  isLaptop,
+  ...
+}: {
+  programs = {
+    btop = {
+      enable = false;
+    };
   };
+  home.packages = with pkgs; [
+    (
+      if isLaptop
+      then btop-cuda
+      else btop-rocm
+    )
+  ];
 }
