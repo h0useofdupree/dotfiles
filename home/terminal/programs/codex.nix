26d207ea746f5409ad366c0ca8e3999333fa323d
h0useofdupree <joel.riekemann@gmail.com>
2025-08-16 15:27:26 +0200

    feat(codex): configure for local llm use
    Currently not pleasant

--- home/terminal/programs/codex.nix
+++ home/terminal/programs/codex.nix
@@ -2,7 +2,24 @@
   programs.codex = {
     enable = true;
     settings = {
-      model = "gpt-5";
+      model_provider = "oss";
+      model = "gtp-oss:20b";
+      approval_policy = "untrusted";
+      sandbox_mode = "workspace-write";
+      model_providers = {
+        oss = {
+          name = "Ollama local";
+          baseURL = "http://localhost:11434/v1";
+        };
+      };
+      profiles = {
+        pvo = {
+          model_provider = "oss";
+          model = "gtp-oss:20b";
+          approval_policy = "untrusted";
+          sandbox_mode = "workspace-write";
+        };
+      };
     };
   };
 }
