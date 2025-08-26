d8a3417f115212b9f002fc3c34f1c307bcb61f0f
h0useofdupree <joel.riekemann@gmail.com>
2025-08-26 17:30:27 +0200

    feat(codex): add default model that uses API instead of local ollama + refactor

--- home/terminal/programs/codex.nix
+++ home/terminal/programs/codex.nix
@@ -2,18 +2,32 @@
   programs.codex = {
     enable = true;
     settings = {
-      model_provider = "oss";
-      model = "gtp-oss:20b";
+      model_provider = "openai";
+      model = "gtp-5";
+
       approval_policy = "untrusted";
       sandbox_mode = "workspace-write";
+
       model_providers = {
+        openai = {
+          name = "OpenAI";
+
+          baseURL = "https://api.openai.com/v1";
+        };
         oss = {
           name = "Ollama local";
           baseURL = "http://localhost:11434/v1";
         };
       };
       profiles = {
-        pvo = {
+        main = {
+          model_provider = "openai";
+          model = "gpt-5";
+          approval_policy = "untrusted";
+          sandbox_mode = "workspace-write";
+        };
+
+        local = {
           model_provider = "oss";
           model = "gtp-oss:20b";
           approval_policy = "untrusted";
