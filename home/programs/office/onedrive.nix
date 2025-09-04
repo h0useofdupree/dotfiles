0e4047540c0bdb3eed75b5d125c7ecf1bba325d3
h0useofdupree <joel.riekemann@gmail.com>
2025-09-04 16:32:00 +0200

    fix(office): use correct nix syntax for settings
    the docs are wrong ai

--- home/programs/office/onedrive.nix
+++ home/programs/office/onedrive.nix
@@ -5,17 +5,18 @@
 }: {
   programs.onedrive = {
     enable = true;
-    settings = ''
-      sync_dir = "${config.home.homeDirectory}/PVO/OneDrive"
-      skip_dir = ".*"
-      skip_file = "~*|.~*|*.tmp"
-      monitor_interval = "300"
+    settings = {
+      sync_dir = "${config.home.homeDirectory}/Projects/PVO/OneDrive";
+      skip_dir = ".*";
+      skip_file = "~*|.~*|*.tmp";
+      monitor_interval = "300";
 
-      no_remote_delete = "true"
-      skip_symlinks = "true"
+      no_remote_delete = "true";
+      upload_only = "true";
+      skip_symlinks = "true";
 
-      check_nosync = "false"
-      check_nomount = "false"
-    '';
+      check_nosync = "false";
+      check_nomount = "false";
+    };
   };
 }
