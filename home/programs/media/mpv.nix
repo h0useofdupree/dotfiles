bd11323eddc8c71842adb9a8ecb56db74193b77d
h0useofdupree <joel.riekemann@gmail.com>
2025-09-15 20:25:32 +0200

    feat(mpv): default to hdr/vulkan

--- home/programs/media/mpv.nix
+++ home/programs/media/mpv.nix
@@ -1,7 +1,15 @@
 {pkgs, ...}: {
   programs.mpv = {
     enable = true;
-    defaultProfiles = ["gpu-hq"];
+    defaultProfiles = ["gpu-hq" "hdr"];
     scripts = [pkgs.mpvScripts.mpris];
+
+    profiles = {
+      hdr = {
+        vo = "gpu-next";
+        gpu-api = "vulkan";
+        target-colorspace-hint = true;
+      };
+    };
   };
 }
