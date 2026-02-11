91bfbf303d389175a7831bc5ca45b24fd7a179ad
h0useofdupree <joel.riekemann@gmail.com>
2026-02-11 18:36:46 +0100

    fix(repl): adapt to newer nix versions

--- pkgs/repl/default.nix
+++ pkgs/repl/default.nix
@@ -16,9 +16,10 @@ in
       ;;
       *)
         if [ -z "$1" ]; then
-          nix repl ${repl}
+          nix repl --expr 'import ${repl} { }'
         else
-          nix repl --arg flakePath $(${coreutils}/bin/readlink -f $1 | ${gnused}/bin/sed 's|/flake.nix||') ${repl}
+        FLAKE_PATH=$($(${coreutils}/bin/readlink -f $1 | ${gnused}/bin/sed 's|/flake.nix||'))
+        nix repl --expr "import ${repl} { flakePath = \"$FLAKE_PATH\"; }"
         fi
       ;;
     esac
