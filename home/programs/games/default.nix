d374a8b8b5a945697edb73f6cb53f1dda76192e5
h0useofdupree <joel.riekemann@gmail.com>
2026-02-24 20:54:16 +0100

    feat(home/programs/games): add oversteer to nixus

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -1,20 +1,24 @@
 {
   pkgs,
   inputs,
+  lib,
+  isLaptop,
   ...
 }: {
   imports = [
     ./vesktop.nix
   ];
 
-  home.packages = with pkgs; [
-    (discord.override {
-      withVencord = true;
-    })
-    inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-lazer-bin
-    gamescope
-    # (lutris.override {extraPkgs = p: [p.libnghttp2];})
-    winetricks
-    # protontricks
-  ];
+  home.packages = with pkgs;
+    [
+      (discord.override {
+        withVencord = true;
+      })
+      inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-lazer-bin
+      gamescope
+      # (lutris.override {extraPkgs = p: [p.libnghttp2];})
+      winetricks
+      # protontricks
+    ]
+    ++ lib.optional (!isLaptop) oversteer;
 }
