608db5613f70f2cee5c1bb26263ee9a2684af5b7
h0useofdupree <joel.riekemann@gmail.com>
2026-03-20 17:10:54 +0100

    feat(system/network): enable WireGuard interface for NordVPN

--- system/network/default.nix
+++ system/network/default.nix
@@ -1,9 +1,26 @@
-{pkgs, ...}: {
+{config, ...}: {
   networking = {
     # nameservers = {};
     networkmanager = {
       enable = true;
       wifi.powersave = true;
+    };
+    wg-quick.interfaces = {
+      nordvpn = {
+        autostart = false;
+        address = ["10.5.0.2/32"];
+        dns = ["103.86.96.100" "103.86.99.100"];
+
+        privateKeyFile = config.age.secrets.nordvpn-private-key.path;
+
+        peers = [
+          {
+            publicKey = "m0tej5P6pYfBivkJc8yRV4KqQXmM81AChLlzlsOSjSs=";
+            endpoint = "de1188.nordvpn.com:51820";
+            allowedIPs = ["0.0.0.0/0"];
+          }
+        ];
+      };
     };
   };
   services = {
@@ -11,6 +28,5 @@
       enable = true;
       settings.UseDns = true;
     };
-    # TODO: Add vpn support (WireGuard/wgnord/NordLynx/etc)
   };
 }
