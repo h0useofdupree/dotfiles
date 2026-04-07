f8a5a68570564574d5455cc65de4d5010b8cf223
h0useofdupree <joel.riekemann@gmail.com>
2026-04-07 18:09:11 +0200

    fix(pkgs/spotify-folders): add python-snappy dependency

--- pkgs/spotify-folders/default.nix
+++ pkgs/spotify-folders/default.nix
@@ -2,14 +2,16 @@
   fetchurl,
   python3,
   writeShellApplication,
-}:
-writeShellApplication {
-  name = "spotify-folders";
-  runtimeInputs = [python3];
-  text = ''
-    python3 ${fetchurl {
-      url = "https://raw.githubusercontent.com/mikez/spotify-folders/refs/heads/master/folders.py";
-      sha256 = "0ym61ryr85kipfn59dpsq81sd9azym1nzc7kxf7brz9dkhqvxpg3";
-    }} "$@"
-  '';
-}
+}: let
+  pythonWithSnappy = python3.withPackages (ps: [ps.python-snappy]);
+in
+  writeShellApplication {
+    name = "spotify-folders";
+    runtimeInputs = [pythonWithSnappy];
+    text = ''
+      python3 ${fetchurl {
+        url = "https://raw.githubusercontent.com/mikez/spotify-folders/refs/heads/master/folders.py";
+        sha256 = "0ym61ryr85kipfn59dpsq81sd9azym1nzc7kxf7brz9dkhqvxpg3";
+      }} "$@"
+    '';
+  }
