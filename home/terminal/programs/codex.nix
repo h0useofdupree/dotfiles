645ae5a5ca037016fcf31927d78a6c9d105e05eb
h0useofdupree <joel.riekemann@gmail.com>
2026-04-09 22:16:03 +0200

    fix(home/terminal/programs/codex): delete reserved 'openai' provider
    reserved for codex default provider

--- home/terminal/programs/codex.nix
+++ home/terminal/programs/codex.nix
@@ -9,23 +9,18 @@
       sandbox_mode = "workspace-write";
 
       model_providers = {
-        openai = {
-          name = "OpenAI";
-
-          baseURL = "https://api.openai.com/v1";
-        };
         oss = {
           name = "Ollama local";
           baseURL = "http://localhost:11434/v1";
         };
       };
       profiles = {
-        main = {
-          model_provider = "openai";
-          model = "gpt-5";
-          approval_policy = "untrusted";
-          sandbox_mode = "workspace-write";
-        };
+        # main = {
+        #   model_provider = "openai";
+        #   model = "gpt-5";
+        #   approval_policy = "untrusted";
+        #   sandbox_mode = "workspace-write";
+        # };
 
         local = {
           model_provider = "oss";
