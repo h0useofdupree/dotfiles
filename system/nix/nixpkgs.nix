1469ef57c3967a66262d87a3a99e129476a177e5
h0useofdupree <joel.riekemann@gmail.com>
2026-04-10 00:35:44 +0200

    fix(system/nix): override material-symbols name lookup to prevent extreme lag/load times

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -18,6 +18,18 @@
             colors = import "${self}/lib/colors" prev.lib;
           };
       })
+      # NOTE: Temporary workaround for Material Symbols variable font lookup lag in Caelestia/Quickshell.
+      (final: prev: {
+        material-symbols = prev.material-symbols.overrideAttrs (old: {
+          postInstall =
+            (old.postInstall or "")
+            + ''
+              ln -s "$out/share/fonts/TTF/MaterialSymbolsRounded.ttf" "$out/share/fonts/TTF/MaterialSymbolsRounded[FILL,GRAD,opsz,wght].ttf"
+              ln -s "$out/share/fonts/TTF/MaterialSymbolsOutlined.ttf" "$out/share/fonts/TTF/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].ttf"
+              ln -s "$out/share/fonts/TTF/MaterialSymbolsSharp.ttf" "$out/share/fonts/TTF/MaterialSymbolsSharp[FILL,GRAD,opsz,wght].ttf"
+            '';
+        });
+      })
     ];
   };
 }
